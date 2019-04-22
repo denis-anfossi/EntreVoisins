961ec8bd62efc0a46dbe821c32714117b909d5ad
Denis Anfossi <denis.anfossi.job@gmail.com>
2019-04-22 23:26:02 +0200

    Update @before NeighbourDetailsActivityTest
    Signed-off-by: Denis Anfossi <denis.anfossi.job@gmail.com>

--- Entrevoisins/app/src/androidTest/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighbourDetailsActivityTest.java
+++ Entrevoisins/app/src/androidTest/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighbourDetailsActivityTest.java
@@ -6,6 +6,7 @@ import android.support.test.runner.AndroidJUnit4;
 
 import com.openclassrooms.entrevoisins.R;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,16 +15,26 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.core.IsNull.notNullValue;
 
 @RunWith(AndroidJUnit4.class)
 public class NeighbourDetailsActivityTest {
 
+    private ListNeighbourActivity mActivity;
+
     @Rule
     public ActivityTestRule<ListNeighbourActivity> mActivityTestRule = new ActivityTestRule<>(ListNeighbourActivity.class);
+
+    @Before
+    public void setUp() {
+        mActivity = mActivityTestRule.getActivity();
+        assertThat(mActivity, notNullValue());
+    }
 
     @Test
     public void neighbourDetailsActivity_isDisplayed() {
