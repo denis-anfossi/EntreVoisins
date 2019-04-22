e680c2e613ed83d6a2b414ec1bfdef1d7f6e0f91
Denis Anfossi <denis.anfossi.job@gmail.com>
2019-04-22 23:18:11 +0200

    Tests refactoring
    Signed-off-by: Denis Anfossi <denis.anfossi.job@gmail.com>

--- Entrevoisins/app/src/androidTest/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighboursListTest.java
+++ Entrevoisins/app/src/androidTest/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighboursListTest.java
@@ -1,19 +1,12 @@
 
-package com.openclassrooms.entrevoisins.neighbour_list;
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
 import com.openclassrooms.entrevoisins.R;
-import com.openclassrooms.entrevoisins.ui.neighbour_list.ListNeighbourActivity;
 import com.openclassrooms.entrevoisins.utils.DeleteViewAction;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -26,7 +19,6 @@ import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static com.openclassrooms.entrevoisins.utils.RecyclerViewItemCountAssertion.withItemCount;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.core.IsNull.notNullValue;
@@ -53,34 +45,13 @@ public class NeighboursListTest {
         assertThat(mActivity, notNullValue());
     }
 
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
-    }
-
     /**
      * We ensure that our recyclerview is displaying at least on item
      */
     @Test
     public void myNeighboursList_shouldNotBeEmpty() {
         // First scroll to the position that needs to be matched and click on it.
-        onView(allOf(withId(R.id.list_neighbours),
-                withParent(allOf(withId(R.id.container), childAtPosition(withId(R.id.main_content), 1))),
-                isDisplayed()))
+        onView(allOf(withId(R.id.list_neighbours), isDisplayed()))
                 .check(matches(hasMinimumChildCount(1)));
     }
 
@@ -89,15 +60,11 @@ public class NeighboursListTest {
      */
     @Test
     public void myNeighboursList_deleteAction_shouldRemoveItem() {
-        onView(allOf(withId(R.id.list_neighbours),
-                withParent(allOf(withId(R.id.container), childAtPosition(withId(R.id.main_content), 1))),
-                isDisplayed()))
+        onView(allOf(withId(R.id.list_neighbours), isDisplayed()))
                 .check(withItemCount(ITEMS_COUNT))
                 .perform(actionOnItemAtPosition(2, new DeleteViewAction()));
 
-        onView(allOf(withId(R.id.list_neighbours),
-                withParent(allOf(withId(R.id.container), childAtPosition(withId(R.id.main_content), 1))),
-                isDisplayed()))
+        onView(allOf(withId(R.id.list_neighbours), isDisplayed()))
                 .check(withItemCount(ITEMS_COUNT - 1));
     }
 }
